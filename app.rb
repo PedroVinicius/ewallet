f760fc79eac295a5d09b6c4b023c74e0422fc624
Pedro Vinícius Lourenço da Silva <pedrovinicius777@gmail.com>
2019-02-12 14:25:04 -0200

    Defining basic routes.

---
+++ app.rb
@@ -0,0 +1,29 @@
+class App < Sinatra::Base
+  get '/accounts' do
+    
+  end
+
+  post '/accounts' do
+
+  end
+
+  get '/accounts/:id' do
+
+  end
+
+  put '/accounts/:id' do
+
+  end
+
+  post '/accounts/:id/transfer/:destination_id' do
+
+  end
+
+  post '/users' do
+
+  end
+
+  post '/users/sign_in' do
+
+  end
+end
