5d72fbf5390be6d0cc7a38c7a5a467cd735d1dd0
Pedro Vinícius Lourenço da Silva <pedrovinicius777@gmail.com>
2019-02-16 09:33:42 -0200

    Creating a basic console.

--- init.rb
+++ init.rb
@@ -1,9 +1,14 @@
 require 'bundler'
 require 'dotenv'
+require 'yaml'
 
 Bundler.require(:default)
 Dotenv.load
 
 APPLICATION_ROOT = File.expand_path('..', __FILE__)
+RACK_ENV = ENV.fetch('RACK_ENV', 'development')
+
+db_config = File.read(File.join(APPLICATION_ROOT, 'config', 'database.yml'))
+DB = Sequel.connect(YAML.load(db_config)[RACK_ENV])
 
 Dir.glob(File.join(APPLICATION_ROOT, '{lib,models}', '**', '*.rb'), &method(:require))
