6c8881306cfefb4f3b587ff3458e1e25780cd5d3
Pedro Vinícius Lourenço da Silva <pedrovinicius777@gmail.com>
2019-02-16 19:08:57 -0200

    Adding some useful sequel plugins.

--- init.rb
+++ init.rb
@@ -9,6 +9,9 @@ Dotenv.load
 APPLICATION_ROOT = File.expand_path('..', __FILE__)
 RACK_ENV = ENV.fetch('RACK_ENV', 'development')
 
+Sequel::Model.plugin :timestamps, update_on_create: true
+Sequel::Model.plugin :json_serializer
+
 db_config = File.read(File.join(APPLICATION_ROOT, 'config', 'database.yml'))
 DB = Sequel.connect(YAML.load(db_config)[RACK_ENV])
 
