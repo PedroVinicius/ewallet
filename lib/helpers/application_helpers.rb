f29f567f7e111e624a84f8aab15b938168f4144f
Pedro Vinícius Lourenço da Silva <pedrovinicius777@gmail.com>
2019-02-16 19:11:56 -0200

    Creating helpers for common tasks.

---
+++ lib/helpers/application_helpers.rb
@@ -0,0 +1,28 @@
+module Sinatra
+  module Ewallet
+    module ApplicationHelpers
+      def request_body
+        return unless request.post?
+        request.body.rewind
+
+        ::JSON.parse(request.body.read, symbolize_names: true)
+      end
+
+      def issue_token(key, data = {})
+        default_data = {
+          iss: 'ewallet',
+          iat: Time.now.to_i,
+          exp: Time.now.to_i + 60 * 60 * 12
+        }
+
+        ::JWT.encode(data.merge(default_data), key, 'RS256')
+      end
+
+      def current_user
+        @user = User.first(id: request.env['user'])
+      end
+    end
+  end
+
+  helpers Ewallet::ApplicationHelpers
+end
