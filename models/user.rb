4828fc7ba11bbc6e67e6aa334cdc09329232da3d
Pedro Vinícius Lourenço da Silva <pedrovinicius777@gmail.com>
2019-02-16 11:17:18 -0200

    Setting up secure passwords generation.

--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,11 @@
 class User < Sequel::Model(:users)
+  include BCrypt
 
+  def password(password)
+    self.encrypted_password
+  end
+  
+  def password=(password)
+    self.encrypted_password = BCrypt::Password.create(password)
+  end
 end
