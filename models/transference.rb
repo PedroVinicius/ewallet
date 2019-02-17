9daa6470aca53e0964ffa14c3faffa2377eeb8be
Pedro Vinícius Lourenço da Silva <pedrovinicius777@gmail.com>
2019-02-17 17:29:18 -0300

    Removing transactions table, as it is not really necessary for now.

--- models/transference.rb
+++
@@ -1,8 +0,0 @@
-class Transference < Sequel::Model(:transferences)
-  many_to_one :transferor, class: :Account, key: :source_account_id
-  many_to_one :transferee, class: :Account, key: :destination_account_id
-
-  def amount
-    values[:amount].to_f
-  end
-end
