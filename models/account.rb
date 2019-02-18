0dcd4b535cab9f052e2ca195b310175e447fad70
Pedro Vinícius Lourenço da Silva <pedrovinicius777@gmail.com>
2019-02-18 18:49:24 -0300

    Adding validations to models.

--- models/account.rb
+++ models/account.rb
@@ -4,16 +4,21 @@ class Account < Sequel::Model(:accounts)
 
   one_to_many :deposits
   one_to_many :withdrawals
-  one_to_many :sent_transferences, class: :Transference, key: :source_account_id
-  one_to_many :received_transferences, class: :Transference, key: :destination_account_id
 
   def balance
     deposits = self.deposits_dataset.sum(:amount) || 0
     withdrawals = self.withdrawals_dataset.sum(:amount) || 0
 
-    sent_transferences_amount = sent_transferences_dataset.sum(:amount) || 0
-    received_transferences_amount = received_transferences_dataset.sum(:amount) || 0
+    deposits - withdrawals
+  end
 
-    ((deposits + received_transferences_amount) - (withdrawals + sent_transferences_amount))
+  def deposit(amount)
+    self.add_deposit(amount: amount)
+  end
+
+  def withdraw(amount)
+    raise NotEnoughtBalanceError if amount > balance
+
+    self.add_withdrawal(amount: amount)
   end
 end
--- models/deposit.rb
+++ models/deposit.rb
@@ -1,7 +1,18 @@
 class Deposit < Sequel::Model(:deposits)
   many_to_one :account
 
+  def validate
+    super
+
+    errors.add(:amount, "can't be empty") if is_amount_nil?
+  end
+
   def amount
     values[:amount].to_f
+  end
+
+  private
+  def is_amount_nil?
+    self.amount.nil?
   end
 end
